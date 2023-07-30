031302e95623aca7c95afef2528d8f72e72cb07f
1Zamuken1 <juanbarrios072@gmail.com>
2023-07-30 11:07:20 -0500

    Create tasks endpoint

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,19 +1,31 @@
+import {pool} from'../db.js'
+
 export const getTasks =(req, res) => {
-    res.send('Obteniendo tareas')
+    res.send('Obteniendo tareas');
 }
 
 export const getTask =(req, res) => {
-    res.send('Obteniendo una tarea')
+    res.send('Obteniendo una tarea');
 }
 
-export const createTask =(req, res) => {
-    res.send('Creando tareas')
-}
+export const createTask = async (req, res) => {
+    const {title, description} = req.body;
+    const [result] = await pool.query(
+    'INSERT INTO tasks(title, description) VALUES (?, ?)',
+    [title,
+    description]
+    );
+console.log(result);
+res.json({
+    id: result.insertId,
+    title, description
+});
+};
 
 export const updateTask =(req, res) => {
-    res.send('Actualizando tarea')
+    res.send('Actualizando tarea');
 }
 
 export const deleteTask =(req, res) => {
-    res.send('Borrando tarea')
+    res.send('Borrando tarea');
 }
