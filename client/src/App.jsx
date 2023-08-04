14d34f423ef5e8ccf84e32f03f52548fd7f8d8f0
Juan Barrios <juanbarrios072@gmail.com>
2023-08-04 11:31:52 -0500

    create task frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import {Route, Routes} from "react-router-dom";
+
 import TasksPage from "./pages/TasksPage";
 import TaskForm from "./pages/TaskForm";
 import NotFound from "./pages/NotFoundPafe";
 
+import NavBar from "./components/NavBar";
+
 function App(){
   return(
+    <>
+    <NavBar />
     <Routes>
       <Route path="/" element={<TasksPage />} />
       <Route path="/new" element={<TaskForm />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    </>
   );
 }
 
--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,6 +1,51 @@
+import { Form, Formik } from 'formik';
+import { CreateTaskRequest } from '../api/tasks.api';
+
 function TaskForm(){
     return(
-        <div>TaskForm</div>
+        <div>
+            <Formik
+                initialValues={
+                    {title:"", 
+                    description:""}
+                }
+                onSubmit={ async (values, actions) => {
+                    console.log(values);
+                    try {
+                        const response = await CreateTaskRequest(values);
+                        console.log(response);
+                        actions.resetForm();
+                    } catch (error) {
+                        console.error(error);
+                        
+                    }
+                }}
+            >
+                {({handleChange, handleSubmit, values, isSubmitting}) => (
+                    <Form onSubmit={handleSubmit}>
+                        <label>title</label>
+                        <input type="text" name="title"
+                        placeholder="Write a title"
+                        onChange={handleChange}
+                        value={values.title}
+                        />
+
+                        <label>description</label>
+                        <textarea 
+                        name="description" 
+                        rows="3" 
+                        placeholder="Write a description"
+                        onChange={handleChange}
+                        value={values.description}
+                        ></textarea>
+
+                        <button type='submit' disabled={isSubmitting}>
+                            {isSubmitting ? "Saving . . ." : "Save"}
+                        </button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
     )
 }
 
